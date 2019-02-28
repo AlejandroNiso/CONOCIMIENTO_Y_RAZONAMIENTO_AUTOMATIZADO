6cce6d6f16fd5c6f0cb1f30d8f3effb35305fa78
Alvaro Zamorano <alvaro.zamorano@edu.uah.es>
2019-02-28 13:20:50 +0100

    Programa Java

--- PECL1/Akinator/src/akinator/Akinator.java
+++ PECL1/Akinator/src/akinator/Akinator.java
@@ -3,6 +3,8 @@ package akinator;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 /**
  *
@@ -10,12 +12,23 @@ import java.io.OutputStream;
  */
 public class Akinator {
 
-    public static void main(String[] args) {
+    
+    
+    public static void main(String[] args) throws SocketException {
+        DatagramSocket serverSocket = null;
+        Interfaz interfaz;
+        
+        try{
+            serverSocket = new DatagramSocket(5008);
+        }catch(SocketException e){
+            
+        }
+       interfaz = new Interfaz(serverSocket);
         java.awt.EventQueue.invokeLater(() -> {
-            new Interfaz().setVisible(true);
+            interfaz.setVisible(true);          
         });
+        
+        UDP udp = new UDP(serverSocket,interfaz);
+        udp.start();
     }
-    
-    
-
 }
---
+++ PECL1/Akinator/src/akinator/UDP.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akinator;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Álvaro Zamorano
+ */
+public class UDP extends Thread{
+    Interfaz interfaz;
+    DatagramSocket socket;
+    
+    public UDP(DatagramSocket socket, Interfaz interfaz){
+        this.interfaz = interfaz;
+        this.socket = socket;
+    }
+    
+    @Override
+    public void run(){
+        boolean continuar = true;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("swipl ./../../../exe.pl");
+            p.waitFor();
+        } catch (IOException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        while(continuar){
+            try{
+                byte[] receiveData = new byte[1];
+            
+                while(true){
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+                    socket.receive(receivePacket);
+                    String sentence = new String(receivePacket.getData());
+                    System.out.println("Received: " + sentence);
+                    InetAddress IPAddress = receivePacket.getAddress();       
+            }
+            
+            } catch (SocketException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+}
