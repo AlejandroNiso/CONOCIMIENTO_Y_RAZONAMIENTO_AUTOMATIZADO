f83daa6e4e20eb207a19f4272ef22225a021943a
Alvaro Zamorano <alvaro.zamorano@edu.uah.es>
2019-03-05 13:13:04 +0100

    hahha

--- PECL1/exe.pl
+++ PECL1/exe.pl
@@ -33,12 +33,25 @@ gameLoop([PrimeraPregunta|RestoPreguntas],ListaRespuestas,ListaLenguajes,Indice)
                                                       read(NombreNuevo),
                                                       caracteristicas(ListaPreguntas),
                                                       nl,write('Deberá contestar unas respuestas extra!'),nl,
-                                                      completarRespuestas(ListaPreguntas,NuevaListaRespuestas,[],ListaGuardar),
+                                                      rellenarRespuestas(ListaPreguntas,NuevaListaRespuestas,NuevaListaRespuestas,0,ListaGuardar),
                                                       meterLenguaje(NombreNuevo, ListaGuardar),!;
                                                       write('Habría sido bueno colaborar!!!'),!);
                           gameLoop(RestoPreguntas,NuevaListaRespuestas,NuevaListaLenguajes,Indice1)))).
 
 gameLoop(_,_,_,_):-write('No quedan preguntas').
+
+
+rellenarRespuestas([PrimeraPregunta|RestoPreguntas],[PrimeraRespuesta|RestoRespuestas],ListaRespuestas,Indice,ListaRetorno):-
+    (PrimeraRespuesta==n-> write('¿Su lenguaje '),write(PrimeraPregunta),write('?'),
+                           read(Respuesta),
+                           write(ListaGuardar),nl,
+                           reemplazar(ListaRespuestas,Indice,Respuesta,ListaGuardar),
+                           Indice2 is Indice +1,
+                           rellenarRespuestas(RestoPreguntas,RestoRespuestas,ListaGuardar,Indice2,ListaRetorno);
+                           Indice2 is Indice +1,
+                           rellenarRespuestas(RestoPreguntas,RestoRespuestas,ListaRespuestas,Indice2,ListaRetorno)).
+rellenarRespuestas(_,_,ListaGuardar,_,ListaGuardar).
+
 
 %Función para rellenar la lista de respuestas al introducir un nuevo lenguaje
 completarRespuestas([PrimeraPregunta|RestoPreguntas],[PrimeraRespuesta|RestoRespuestas],ListaGuardar,ListaRetorno):-
@@ -49,7 +62,7 @@ completarRespuestas([PrimeraPregunta|RestoPreguntas],[PrimeraRespuesta|RestoResp
                            completarRespuestas(RestoPreguntas,RestoRespuestas,[Respuesta|ListaGuardar],ListaRetorno);
                            completarRespuestas(RestoPreguntas,RestoRespuestas,[PrimeraRespuesta|ListaGuardar],ListaRetorno)).
 
-completarRespuestas(_,_,ListaGuardar,ListaGuardar).
+%completarRespuestas(_,_,ListaGuardar,ListaGuardar).
 
 %Comparar caracteristicas y quitar lenguaje de la lista general
 validar(ListaRespuestas,[Lenguaje1|RestoLenguajes],FinalAnterior,Final):-
@@ -120,4 +133,5 @@ send(Message) :-
         udp_send(S, Message, localhost:5008, []),
         tcp_close_socket(S).
         
-:-akinator.
+%:-akinator.
+%completarRespuestas(ListaPreguntas,NuevaListaRespuestas,[],ListaGuardar),
