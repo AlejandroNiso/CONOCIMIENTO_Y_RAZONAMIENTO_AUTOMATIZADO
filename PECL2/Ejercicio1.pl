02c85e4b6d46e1c3ba1b59dbf2299698dfbf4e27
Alvaro Zamorano <alvaro.zamorano@edu.uah.es>
2019-03-14 09:08:18 +0100

    PL3

---
+++ PECL2/Ejercicio1.pl
@@ -0,0 +1,44 @@
+% Autor:
+% Fecha: 14/03/2019
+
+% Reglas gramaticales
+oracion --> g_nominal, g_verbal.
+g_nominal --> nombre.
+g_nominal --> nombre, adjetivo.
+g_nominal --> determinante, nombre.
+g_nominal --> determinante, nombre,adjetivo.
+
+g_verbal --> verbo.
+g_verbal --> verbo, g_nominal.
+g_verbal --> verbo, adjetivo.
+g_verbal --> verbo, adjetivo.
+g_verbal --> verbo, g_adjetival.
+g_verbal --> verbo, g_preposicional.
+
+g_preposicional --> preposicion,nombre.
+g_preposicional --> preposicion,determinante,nombre.
+
+g_adjetival --> determinante,nombre,adjetivo.
+%Diccionario
+determinante --> [el].
+determinante --> [la].
+determinante --> [un].
+determinante --> [una].
+nombre --> [hombre].
+nombre --> [mujer].
+nombre --> [juan].
+nombre --> [maria].
+nombre --> [manzana].
+nombre --> [manzanas].
+nombre --> [gato].
+nombre --> [raton].
+nombre --> [alumno].
+nombre --> [universidad].
+verbo --> [ama].
+verbo --> [come].
+verbo --> [estudia].
+adjetivo --> [roja].
+adjetivo --> [negro].
+adjetivo --> [grande].
+adjetivo --> [gris].
+adjetivo --> [pequeno].
