534fddf279111ab1d1fc9a8e6b19873e131fa748
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-04-19 13:38:20 +0200

    pruebas recursion suma

--- PECL3/pecl3.rkt
+++ PECL3/pecl3.rkt
@@ -21,5 +21,19 @@
 
 (define nil (lambda (z) z))
 
-(define lista-funciones ((par ((par primero) primero)) ((par ((par primero) segundo)) ((par ((par segundo) primero)) ((par ((par segundo) segundo)) nil)))))
+;;(define _lista-funciones ((par ((par primero) primero)) ((par ((par primero) segundo)) ((par ((par segundo) primero)) ((par ((par segundo) segundo)) nil)))))
 
+(define lista-funciones ((par false) ((par ((par primero) primero)) ((par false) ((par ((par primero) segundo)) ((par false) ((par ((par segundo) primero)) ((par false) ((par ((par segundo) segundo)) nil)))))))))
+
+(define _suma-matrices (lambda (m1)
+                         (lambda (m2)
+                           (lambda (mod)
+                             (lambda (lista)
+                               ((esnil lista) matriz ((((_suma-matrices m1) m2) mod) (segundo (segundo lista-funciones)))))))))
+
+;;((((_suma-matrices m1) m2) mod) (segundo(segundo lista)))
+
+(define suma-matrices (lambda (m1)
+                        (lambda (m2)
+                          (lambda (mod)
+                            ((((_suma-matrices m1) m2) mod) lista-funciones)))))
